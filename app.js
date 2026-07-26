dc12d8f59cd92811b486073f28b4caf98e23db6c
nzeenath <nzeenath2002@gmail.com>
2026-07-26 23:56:58 +0530

    2nd commit

--- app.js
+++ app.js
@@ -13,21 +13,44 @@
 
 // console.log(custName);
 
-let customer = {
+// let customer = {
 
-      name: "Kamal",
-    age: 12,
-    isActive: true
+//       name: "Kamal",
+//     age: 12,
+//     isActive: true
+// }
+
+// let stringCustomer = JSON.stringify(customer);
+
+
+
+// localStorage.setItem("customer" ,stringCustomer);
+
+// let retCustomer = localStorage.getItem("customer");
+
+// let jsonCustomer = JSON.parse(retCustomer);
+
+// console.log(jsonCustomer);
+
+
+
+const customerList = JSON.parse(localStorage.getItem("customerList"));
+
+function btnAddCustomerOnAction() {
+    //  let customerList = JSON.parse(localStorage.getItem("customerList"));
+    let customer = {
+        id: document.getElementById("txtCustomerId").value,
+        name: document.getElementById("txtCustomerName").value,
+        age: document.getElementById("txtCustomerAge").value,
+        address: document.getElementById("txtCustomerAddress").value
+    }
+    console.log(customer);
+
+    customerList.push(customer);
+
+    localStorage.setItem("customerList", JSON.stringify(customerList));
+    btnLoadTableOnAction();
+
+    // alert("add customer")
 }
 
-let stringCustomer = JSON.stringify(customer);
-
-
-
-localStorage.setItem("customer" ,stringCustomer);
-
-let retCustomer = localStorage.getItem("customer");
-
-let jsonCustomer = JSON.parse(retCustomer);
-
-console.log(jsonCustomer);
